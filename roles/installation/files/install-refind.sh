8755b57599c246a92dd5cbf46f9ebc4f6b203874
Andrew Grechkin <andrew.grechkin@gmail.com>
2020-11-12 21:13:33 +0100

    tune installation 1

--- roles/installation/files/install-refind.sh
+++ roles/installation/files/install-refind.sh
@@ -2,11 +2,20 @@
 
 #arch-chroot /mnt refind-install --root /mnt/boot/EFI
 arch-chroot /mnt refind-install --usedefault /dev/disk/by-label/EFI
+
+if [ "$1" = "true" ]; then
+{
+	echo '"Boot with crypt"    "rw root=LABEL=root resume=LABEL=swap nowatchdog cryptdevice=LABEL=lvm-encrypted:lvm:allow-discards"'
+	echo '"Boot with defaults" "rw root=LABEL=root resume=LABEL=swap nowatchdog splash quiet udev.log_priority=3"'
+	echo '"Boot to terminal"   "rw root=LABEL=root resume=LABEL=swap systemd.unit=multi-user.target"'
+} >> /mnt/boot/refind_linux.conf
+else
 {
 	echo '"Boot with defaults" "rw root=LABEL=root resume=LABEL=swap nowatchdog splash quiet udev.log_priority=3"'
 	echo '"Boot to terminal"   "rw root=LABEL=root resume=LABEL=swap systemd.unit=multi-user.target"'
 	echo '"Boot with crypt"    "rw root=LABEL=root resume=LABEL=swap nowatchdog cryptdevice=LABEL=lvm-encrypted:lvm:allow-discards"'
 } >> /mnt/boot/refind_linux.conf
+fi
 
 # virtualbox compatibility
 #cp -r /mnt/boot/EFI/refind /mnt/boot/EFI/BOOT
--- roles/installation/files/update-initcpio.sh
+++ roles/installation/files/update-initcpio.sh
@@ -4,9 +4,13 @@ mdadm --detail --scan >> /mnt/etc/mdadm.conf
 
 #HOOKS=(base udev autodetect <keyboard> consolefont modconf block <encrypt lvm2 resume> filesystems fsck)
 
-sed -i 's|MODULES=()|MODULES=(vfat btrfs)|'                                    /mnt/etc/mkinitcpio.conf
-sed -i 's|BINARIES=()|BINARIES=(lsblk vim cfdisk)|'                            /mnt/etc/mkinitcpio.conf
-sed -i 's|block filesystems|block mdadm_udev encrypt lvm2 resume filesystems|' /mnt/etc/mkinitcpio.conf
-sed -i 's|autodetect|autodetect keyboard|'                                     /mnt/etc/mkinitcpio.conf
+sed -i 's|MODULES=()|MODULES=(vfat btrfs)|'                                        /mnt/etc/mkinitcpio.conf
+sed -i 's|BINARIES=()|BINARIES=(lsblk vim cfdisk)|'                                /mnt/etc/mkinitcpio.conf
+sed -i 's|autodetect|autodetect keyboard|'                                         /mnt/etc/mkinitcpio.conf
+if [ "$1" = "true" ]; then
+	sed -i 's|block filesystems|block mdadm_udev encrypt lvm2 resume filesystems|' /mnt/etc/mkinitcpio.conf
+else
+	sed -i 's|block filesystems|block mdadm_udev lvm2 resume filesystems|'         /mnt/etc/mkinitcpio.conf
+fi
 
 arch-chroot /mnt mkinitcpio -n -P
