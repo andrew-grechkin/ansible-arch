fca935a24a1c2b021d51171353c76cd0cd2f163d
Andrew Grechkin <andrew.grechkin@gmail.com>
2020-12-19 22:56:44 +0100

    improve installation

--- roles/system/install/files/install-refind.sh
+++ roles/system/install/files/install-refind.sh
@@ -19,9 +19,6 @@ fi
 
 perl -i -plE "s{^(LABEL=\w+)}{\1$2}" /mnt/etc/fstab
 
-CWD="$(dirname "$0")"
-cp -r "$CWD/icons"      "/mnt/boot/EFI/refind/"
-cp -r "$CWD/next-theme" "/mnt/boot/EFI/refind/"
 echo "include next-theme/theme.conf" >> "/mnt/boot/EFI/refind/refind.conf"
 
 ### virtualbox compatibility
