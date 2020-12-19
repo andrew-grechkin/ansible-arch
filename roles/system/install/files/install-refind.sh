14c4c81406810738b21a28b121894ad086bb8aa3
Andrew Grechkin <andrew.grechkin@gmail.com>
2020-12-19 22:24:18 +0100

    improve installation

--- roles/system/install/files/install-refind.sh
+++ roles/system/install/files/install-refind.sh
@@ -8,18 +8,24 @@ if [ "$1" = "True" ]; then
 	echo "\"Boot with crypt\"    \"rw root=LABEL=root$2 resume=LABEL=swap$2 nowatchdog splash quiet udev.log_priority=3 cryptdevice=LABEL=lvm-encrypted${2}:lvm:allow-discards\""
 	echo "\"Boot with defaults\" \"rw root=LABEL=root$2 resume=LABEL=swap$2 nowatchdog splash quiet udev.log_priority=3\""
 	echo "\"Boot to terminal\"   \"rw root=LABEL=root$2 resume=LABEL=swap$2 systemd.unit=multi-user.target\""
-} >> /mnt/boot/refind_linux.conf
+} > /mnt/boot/refind_linux.conf
 else
 {
 	echo "\"Boot with defaults\" \"rw root=LABEL=root$2 resume=LABEL=swap$2 nowatchdog splash quiet udev.log_priority=3\""
 	echo "\"Boot with crypt\"    \"rw root=LABEL=root$2 resume=LABEL=swap$2 nowatchdog splash quiet udev.log_priority=3 cryptdevice=LABEL=lvm-encrypted${2}:lvm:allow-discards\""
 	echo "\"Boot to terminal\"   \"rw root=LABEL=root$2 resume=LABEL=swap$2 systemd.unit=multi-user.target\""
-} >> /mnt/boot/refind_linux.conf
+} > /mnt/boot/refind_linux.conf
 fi
 
 perl -i -plE "s{^(LABEL=\w+)}{\1$2}" /mnt/etc/fstab
 
+CWD="$(dirname "$0")"
+cp -r "$CWD/icons"      "/mnt/boot/EFI/refind/"
+cp -r "$CWD/next-theme" "/mnt/boot/EFI/refind/"
+echo "include next-theme/theme.conf" >> "/mnt/boot/EFI/refind/refind.conf"
+
 ### virtualbox compatibility
 [[ -d /mnt/boot/EFI/BOOT ]] || {
 	cp -r /mnt/boot/EFI/refind /mnt/boot/EFI/BOOT
+	mv /mnt/boot/EFI/BOOT/refind_x64.efi /mnt/boot/EFI/BOOT/bootx64.efi
 }
