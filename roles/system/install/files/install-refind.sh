fe5cc3e437e71a3605eb95e80d4ec8c81f1ccb1f
Andrew Grechkin <andrew.grechkin@gmail.com>
2020-12-21 21:47:32 +0100

    improve installation

--- roles/system/install/files/install-refind.sh
+++ roles/system/install/files/install-refind.sh
@@ -3,19 +3,33 @@
 refind-install --root /mnt
 # arch-chroot /mnt refind-install --usedefault "/dev/disk/by-label/EFI$2"
 
+CPU=$(lscpu)
+if [[ $CPU =~ GenuineIntel ]]; then
+	arch-chroot /mnt pacman -S --needed --noconfirm intel-ucode
+	INITRD_U='initrd=\\intel-ucode.img'
+else
+	INITRD_U=''
+fi
+
+INITRD_K='initrd=\initramfs-linux.img'
+DRIVES="root=LABEL=root$2 resume=LABEL=swap$2"
+OPTIONS="nowatchdog splash quiet udev.log_priority=3"
+CRYPT="cryptdevice=LABEL=lvm-encrypted${2}:lvm:allow-discards"
+
 if [ "$1" = "True" ]; then
 {
-	echo "\"Boot with crypt\"    \"rw root=LABEL=root$2 resume=LABEL=swap$2 nowatchdog splash quiet udev.log_priority=3 cryptdevice=LABEL=lvm-encrypted${2}:lvm:allow-discards\""
-	echo "\"Boot with defaults\" \"rw root=LABEL=root$2 resume=LABEL=swap$2 nowatchdog splash quiet udev.log_priority=3\""
-	echo "\"Boot to terminal\"   \"rw root=LABEL=root$2 resume=LABEL=swap$2 systemd.unit=multi-user.target\""
+	echo "\"Boot with crypt+ucode\" \"rw $DRIVES $OPTIONS $CRYPT $INITRD_U $INITRD_K\""
 } > /mnt/boot/refind_linux.conf
 else
 {
-	echo "\"Boot with defaults\" \"rw root=LABEL=root$2 resume=LABEL=swap$2 nowatchdog splash quiet udev.log_priority=3\""
-	echo "\"Boot with crypt\"    \"rw root=LABEL=root$2 resume=LABEL=swap$2 nowatchdog splash quiet udev.log_priority=3 cryptdevice=LABEL=lvm-encrypted${2}:lvm:allow-discards\""
-	echo "\"Boot to terminal\"   \"rw root=LABEL=root$2 resume=LABEL=swap$2 systemd.unit=multi-user.target\""
+	echo "\"Boot with ucode\"       \"rw $DRIVES $OPTIONS $INITRD_U $INITRD_K\""
 } > /mnt/boot/refind_linux.conf
 fi
+{
+	echo "\"Boot with crypt\"       \"rw $DRIVES $OPTIONS $CRYPT\""
+	echo "\"Boot with defaults\"    \"rw $DRIVES $OPTIONS\""
+	echo "\"Boot to terminal\"      \"rw $DRIVES systemd.unit=multi-user.target\""
+} >> /mnt/boot/refind_linux.conf
 
 perl -i -plE "s{^(LABEL=\w+)}{\1$2}" /mnt/etc/fstab
 
