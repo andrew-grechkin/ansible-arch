1c96ffb1ab793d03cd973d514408ff75b2c69ac1
Andrew Grechkin <andrew.grechkin@gmail.com>
2024-01-05 20:43:18 +0100

    update roles

--- roles/system/grub/files/prepare-grub.sh
+++ roles/system/grub/files/prepare-grub.sh
@@ -4,6 +4,6 @@ rm -f  /boot/*.png
 rm -rf /boot/EFI/BOOT
 rm -rf /boot/EFI/tools
 
-grub-install --removable --efi-directory=/boot
+grub-install --removable --efi-directory=/boot --force
 
 grub-mkconfig -o /boot/grub/grub.cfg
