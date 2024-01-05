32aea51774e7c86bf68a5ebc1ad23f512346fc15
Andrew Grechkin <andrew.grechkin@gmail.com>
2024-01-05 21:43:04 +0100

    update roles

--- roles/system/grub/files/prepare-grub.sh
+++ roles/system/grub/files/prepare-grub.sh
@@ -3,9 +3,11 @@
 set -e
 
 if [[ -d "/efi" ]]; then
-	MNT="$(grep '/efi' < /etc/fstab | awk '{print $1}')"
+	MNT="$(grep '/efi' /etc/fstab | awk '{print $1}')"
 	if [[ -n "$MNT" ]]; then
-		sed -i 's|\(GRUB_CMDLINE_LINUX_DEFAULT="\)|\1resume='"$MNT"' |' /etc/default/grub
+		if ! grep 'GRUB_CMDLINE_LINUX_DEFAULT.+resume=' /etc/default/grub; then
+			sed -i 's|\(GRUB_CMDLINE_LINUX_DEFAULT="\)|\1resume='"$MNT"' |' /etc/default/grub
+		fi
 
 		rm -f  /boot/*.png
 		rm -rf /boot/EFI/BOOT
